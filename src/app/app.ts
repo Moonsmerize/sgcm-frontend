e93efd42981b06dcbd9ac6fd51985710d7d05554
Moonsmerize <luan991019@gmail.com>
2025-10-26 22:40:25 -0600

    elemento dinamico agregado al footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,33 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, OnInit, OnDestroy } from '@angular/core';
 // Importa RouterOutlet y RouterLink
 import { RouterOutlet, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true, // Asegúrate que sea true
   imports: [
-    RouterOutlet, // Añade RouterOutlet aquí
-    RouterLink    // Añade RouterLink aquí
+    CommonModule,
+    RouterOutlet,
+    RouterLink
     ], 
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit, OnDestroy {
   protected readonly title = signal('sgcm-frontend');
+  protected currentTime = signal(new Date());
+  private intervalId?: number;
+
+  ngOnInit(): void {
+    this.intervalId = window.setInterval(() => {
+      this.currentTime.set(new Date());
+    }, 1000);
+  }
+
+  ngOnDestroy(): void {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
 }
